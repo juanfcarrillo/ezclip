87813968c71f2b42a567aa3057531a8311095df7
juanfcarrillo <juanfi444@outlook.com>
2025-07-28 12:32:43 -0500

    chore(firebase): added initial conf

--- firebase_init.py
+++ firebase_init.py
@@ -1,9 +1,15 @@
 import os
 from firebase_admin import initialize_app
+import firebase_admin.credentials as credentials
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
+FIREBASE_CREDENTIALS_PATH = os.getenv("FIREBASE_CREDENTIALS_PATH")
 FIREBASE_PROJECT_ID = os.getenv("FIREBASE_PROJECT_ID")
-firebase_app = initialize_app(cred, {"projectId": FIREBASE_PROJECT_ID})
+
+cred = credentials.Certificate(FIREBASE_CREDENTIALS_PATH)
+firebase_app = initialize_app(cred, {
+    'projectId': FIREBASE_PROJECT_ID
+})
