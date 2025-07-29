76c0d42faf9858a8dd64f8b97ea1847f66d9696b
juanfcarrillo <juanfi444@outlook.com>
2025-07-29 03:22:27 -0500

    chore: added config files

--- app/main/app.py
+++ app/main/app.py
@@ -1,8 +1,13 @@
+
 from fastapi import FastAPI
 from app.clipping.router import router as clipping_router
+from firebase_init import firebase_app
 
 
 def create_app() -> FastAPI:
+    # Ensure Firebase is initialized
+    _ = firebase_app
+
     python_app = FastAPI(title="ezclip API")
     # Add routers
     python_app.include_router(clipping_router, prefix="/clipping", tags=["clipping"])
