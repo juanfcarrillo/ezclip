53c73a54d17b7a6ede8ed527a57974cf10c8480e
juanfcarrillo <juanfi444@outlook.com>
2025-07-29 03:22:07 -0500

    fix(firebase): changed env variables

--- firebase_init.py
+++ firebase_init.py
@@ -9,7 +9,15 @@ load_dotenv()
 FIREBASE_CREDENTIALS_PATH = os.getenv("FIREBASE_CREDENTIALS_PATH")
 FIREBASE_PROJECT_ID = os.getenv("FIREBASE_PROJECT_ID")
 
-cred = credentials.Certificate(FIREBASE_CREDENTIALS_PATH)
-firebase_app = initialize_app(cred, {
-    'projectId': FIREBASE_PROJECT_ID
-})
+
+def initialize_firebase_app():
+    cred = credentials.Certificate(FIREBASE_CREDENTIALS_PATH)
+
+    app = initialize_app(cred, {
+        'projectId': FIREBASE_PROJECT_ID
+    })
+
+    return app
+
+
+firebase_app = initialize_firebase_app()
